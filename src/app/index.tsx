f1fa510560e982f73c9eb2567f3c7c1b2ec13116
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-02-25 16:00:13 -0300

    feat: add HomeHeader component with linear gradient and update styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
+import { HomeHeader } from "@/components/Home";
 import { Text, View, Button } from "react-native";
-import { router } from "expo-router";
-import { fontFamily } from "@/theme/fontFamily";
-
 
 
 export default function index() {
 
     return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text style={{ fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
-            <Button title="Nova Meta" onPress={() => router.navigate("/target")}></Button>
-            <Button title="Transação" onPress={() => router.navigate("/transaction/129222")}></Button>
-            <Button title="Progresso" onPress={() => router.navigate("/in-progress/12")}></Button>
+        <View style={{ flex: 1 }}>
+            <HomeHeader />
         </View>
     )
 }
