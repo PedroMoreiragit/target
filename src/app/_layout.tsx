dbdd6625a052d0cddd89054b61581c33f5498c91
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-02-25 15:52:09 -0300

    feat: add custom font loading and improve layout
    - Added @expo-google-fonts/inter and expo-font dependencies to package.json.
    - Implemented font loading in the Layout component, displaying a loading indicator while fonts are being loaded.
    - Created a Loading component to show a spinner during font loading.
    - Updated index component to use custom font styles for text.
    - Introduced fontFamily utility for managing font styles.
    - Added styles for the Loading component to center the spinner.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,32 @@
 import { Stack } from "expo-router";
 
 import { colors } from "@/theme/colors";
-
+import { useFonts } from "expo-font"
+import {
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold
+} from "@expo-google-fonts/inter";
+import { Loading } from "@/components/Loading";
 
 export default function Layout() {
-    console.log("PASSOU PELO LAYOUT PRIMERO");
+
+    const [fontLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_700Bold
+    })
+
+    if (!fontLoaded) {
+        return (
+            <Loading />
+        )
+    }
+
     return (
         <Stack screenOptions={{
             headerShown: false,
-            contentStyle: {backgroundColor: colors.white}
+            contentStyle: { backgroundColor: colors.white }
         }} />
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,14 @@
-import { router } from "expo-router";
 import { Text, View, Button } from "react-native";
+import { router } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
+
 
 
 export default function index() {
+
     return (
         <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text>Olá, Expo Router</Text>
+            <Text style={{ fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
             <Button title="Nova Meta" onPress={() => router.navigate("/target")}></Button>
             <Button title="Transação" onPress={() => router.navigate("/transaction/129222")}></Button>
             <Button title="Progresso" onPress={() => router.navigate("/in-progress/12")}></Button>
