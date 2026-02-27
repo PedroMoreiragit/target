529a08670af1a4e8fd750c2b31122f8bae2c07bc
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-02-27 18:05:40 -0300

    feat: add CurrencyInput and Progress components, and integrate them into InProgress and Target screens

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,12 +1,58 @@
-import { Button, Text, View } from "react-native";
+import { View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { List } from "@/components/List";
+
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { Button } from "@/components/Button";
+
+const details = {
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+    percentage: 25,
+}
+
+const transactions : TransactionProps[] = [
+    {
+        id: "1",
+        value: "R$ 20,00",
+        date: "12/04/25",
+        type: TransactionTypes.Output,
+    },
+
+    {
+        id: "2",
+        value: "R$ 300,00",
+        date: "12/04/25",
+        description: "CDB de 110% no banco XPTO",
+        type: TransactionTypes.Input,
+    },
+]
 
 export default function InProgress() {
-    const params = useLocalSearchParams<{id: string}>()
+    const params = useLocalSearchParams<{ id: string }>()
     return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text>InProgress: {params.id}</Text>
-            <Button title="Voltar" onPress={() => router.back()}></Button>
+        <View style={{ flex: 1, padding: 24, gap: 32 }}>
+            <PageHeader
+                title="Apple Watch"
+                rightButton={{
+                    icon: "edit",
+                    onPress: () => { },
+                }}
+            />
+
+            <Progress data={details} />
+
+            <List
+                title="Transações"
+                data={transactions}
+                renderItem={({item}) => ( <Transaction data={item} onRemove={() => {}}/>)}
+                emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+            />
+
+            <Button title="Nova Transação" onPress={() => router.navigate(`/transaction/${params.id}`)}/>
         </View>
     )
 }
--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 
+import { CurrencyInput } from "@/components/CurrencyInput";
 import { PageHeader } from "@/components/PageHeader";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
@@ -12,6 +13,9 @@ export default function Target() {
             <View style={{ marginTop: 32, gap: 24 }}>
 
                 <Input label="Nome da meta" placeholder="Ex: Viagem para praia, 5090" />
+
+                <CurrencyInput label="Valor alvo" value={0}/>
+
                 <Button title="Salvar" />
 
             </View>
