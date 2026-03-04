a29f1250df2045789ac2ea94bec9c71ed4936985
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-03-04 14:17:11 -0300

    feat: integrate transaction fetching logic and remove hardcoded transactions in InProgress component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -12,25 +12,9 @@ import { Button } from "@/components/Button";
 import { useTargetDatabase } from "@/database/useTargetDatabase";
 import { numberToCurrency } from "@/utils/numberToCurrency";
 import { Loading } from "@/components/Loading";
+import { useTransactionsDatabase } from "@/database/useTransactionsDatabase";
 
 
-
-const transactions: TransactionProps[] = [
-    {
-        id: "1",
-        value: "R$ 20,00",
-        date: "12/04/25",
-        type: TransactionTypes.Output,
-    },
-
-    {
-        id: "2",
-        value: "R$ 300,00",
-        date: "12/04/25",
-        description: "CDB de 110% no banco XPTO",
-        type: TransactionTypes.Input,
-    },
-]
 
 export default function InProgress() {
     const [isFetching, setIsFetching] = useState(true);
@@ -43,9 +27,11 @@ export default function InProgress() {
     const params = useLocalSearchParams<{ id: string }>()
 
     const targetDatabase = useTargetDatabase();
+    const [transactions, setTransactions] = useState<TransactionProps[]>([])
+    const transactionsDatabase = useTransactionsDatabase();
 
 
-    async function fetchDetails() {
+    async function fetchTargetDetails() {
         try {
             const response = await targetDatabase.show(Number(params.id))
             setDetails({
@@ -60,23 +46,45 @@ export default function InProgress() {
         }
     }
 
-    
+    async function fetchTransitions() {
+        try {
+            const response = await transactionsDatabase.listByTargetId(
+                Number(params.id)
+            )
+
+            setTransactions(
+                response.map((item) => ({
+                    id: String(item.id),
+                    value: numberToCurrency(item.amount),
+                    date: String(item.created_at),
+                    description: item.observation,
+                    type: item.amount < 0 ? TransactionTypes.Output : TransactionTypes.Input
+                }))
+            )
+
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível carregar as transações.")
+            console.log(error)
+        }
+    }
+
 
     async function fetchData() {
-        const fetchDetailsPromise = fetchDetails()
-        await Promise.all([fetchDetailsPromise]);
-       setIsFetching(false);
+        const fetchDetailsPromise = fetchTargetDetails()
+        const fetchTransitionsPromise = fetchTransitions()
+        await Promise.all([fetchDetailsPromise, fetchTransitionsPromise]);
+        setIsFetching(false);
     }
 
     useFocusEffect(
         useCallback(() => { fetchData() }, [])
     )
 
-     if(isFetching){
-            return(
-                <Loading />
-            )
-        }
+    if (isFetching) {
+        return (
+            <Loading />
+        )
+    }
 
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
